8aaf041721ed55af595ddd926f8de5d4dfb19c11
fandipro <fprasetyo4@gmail.com>
2022-07-09 07:59:57 +0700

    setup deploy,db.js serta membuat env

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,25 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
+
+// console.log("test");
 const pool = new Pool({
   user: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-//   ssl: {
-//     rejectUnauthorized: false,
-// }
-})
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  // }
+});
 
-module.exports = pool
+const db = new Pool(pool);
+
+db.connect((err) => {
+  if (err) {
+    console.log("Failed to connect database...", err.message);
+    process.exit(1);
+  }
+  console.log("Successfully connect database...");
+});
+
+module.exports = db;
