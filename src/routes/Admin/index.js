08784ac8808c5e8e02527ca9a46679940f178356
Candra Julius <candrajulius97@gmail.com>
2022-07-11 14:27:45 +0700

    add countries CRUD

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -4,9 +4,11 @@ const upload = require('../../middleware/multer')
 const router = express.Router()
 const airlanes = require('./airlanes')
 const ticket = require('./tiket')
+const country = require('./country')
 
 router
   .use('/airlanes', airlanes)
   .use('/ticket', ticket)
+  .use('/country', country)
   .post('/login', upload.none(), login)
 module.exports = router
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express')
 const router = express.Router()
 const admin = require('./Admin/index')
@@ -6,14 +5,5 @@ const admin = require('./Admin/index')
 
 router
   .use('/admin', admin)
-=======
-const express = require("express");
-const router = express.Router();
-
-// const usersRoute = require('./users')
-
-router;
-// .use('/users', usersRoute)
->>>>>>> f7963a169d1bb6eeafdaf8151a988d3b61977528
 
 module.exports = router;
