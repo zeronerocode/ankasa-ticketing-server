de25521692d8f153fbf43604f28594218bcebacc
Candra Julius <candrajulius97@gmail.com>
2022-07-12 20:19:47 +0700

    debug delete booking

--- src/models/Admin/booking.js
+++ src/models/Admin/booking.js
@@ -10,8 +10,8 @@ const bookingModule = {
     updateBooking: (data) => {
         return pool.query('UPDATE bookings SET title = COALESCE($1, title), full_name = COALESCE($2, full_name), nationality = COALESCE($3, nationality), travel_insurance = COALESCE($4, travel_insurance), payment_status = COALESCE($5, payment_status), total_payment = COALESCE($6, total_payment), updated_at = $7 WHERE id = $8', [data.title, data.full_name, data.nationality, data.insurance, data.payment_status, data.total_payment, data.Updated_at, data.id])
     },
-    deleteBooking: (data) =>{
-        return pool.query('DELETE FROM bookings WHERE id = $1', [data.id])
+    deleteBooking: (id) =>{
+        return pool.query('DELETE FROM bookings WHERE id = $1', [id])
     }
 }
 
