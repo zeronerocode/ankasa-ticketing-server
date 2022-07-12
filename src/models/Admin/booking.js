cdb71c8f967a7ea61dceef79e4cf09eee48f8d8f
Candra Julius <candrajulius97@gmail.com>
2022-07-12 16:28:07 +0700

    CRUD Booking & debug update airlines

--- src/models/Admin/booking.js
+++ src/models/Admin/booking.js
@@ -5,7 +5,7 @@ const bookingModule = {
         return pool.query('SELECT * FROM bookings LIMIT $1 OFFSET $2', [data.limit, data.offset])
     },
     detailBooking: (id) => {
-        return pool.query('SELECT * FROM bookings WHERE id', [id])
+        return pool.query('SELECT * FROM bookings WHERE id = $1', [id])
     },
     updateBooking: (data) => {
         return pool.query('UPDATE bookings SET title = COALESCE($1, title), full_name = COALESCE($2, full_name), nationality = COALESCE($3, nationality), travel_insurance = COALESCE($4, travel_insurance), payment_status = COALESCE($5, payment_status), total_payment = COALESCE($6, total_payment), updated_at = $7 WHERE id = $8', [data.title, data.full_name, data.nationality, data.insurance, data.payment_status, data.total_payment, data.Updated_at, data.id])
