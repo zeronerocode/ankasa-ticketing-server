8a6baeaddd352ba6543bb2bba6c09b3029188459
Candra Julius <candrajulius97@gmail.com>
2022-07-13 05:24:38 +0700

    add flight CRUD

--- src/routes/Admin/index.js
+++ src/routes/Admin/index.js
@@ -6,12 +6,14 @@ const airlanes = require('./airlanes')
 const booking = require('./booking')
 const country = require('./country')
 const users = require('./users')
+const flights = require('./tiket')
 
 router
   .use('/airlanes', airlanes)
   .use('/booking', booking)
   .use('/country', country)
   .use('/users', users)
+  .use('/flight', flights)
   .post('/login', upload.none(), login)
 
 module.exports = router
