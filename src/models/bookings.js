786035d6c4038da0fa9feb3474131b12ab5c0c6c
fandipras7 <fandiprasetyopro@gmail.com>
2022-07-18 10:23:41 +0700

    fixing bug at create bookings

--- src/models/bookings.js
+++ src/models/bookings.js
@@ -7,7 +7,7 @@ const bookingsModel = {
             INSERT INTO bookings (id, user_id, flight_id, title, full_name, nationality, travel_insurance, payment_status, total_payment, totalorder)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
             `, [data.id, data.user_id, data.flight_id, data.title, data.fullname, data.nationality,
-            data.travel_insurance, data.payment_status, data.total_payment, data.totalorder], (err,result)=>{
+            data.travel_insurance, data.payment_status, data.totalPayment, data.totalorder], (err,result)=>{
                 if(!err) {
                     resolve(result)
                 } else {
